e3f54361895a669e72e0b1f80d40384a693bffac
Nicholas Anaya <anaya.nicholas@gmail.com>
2017-01-27 06:09:55 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,10 @@
-
-
+def my_collect(collection)
+  new_array = []
+  i = 0
+  while i < collection.length
+     new_element = yield collection[i]
+     new_array << new_element
+      i += 1
+  end
+  new_array
+end
